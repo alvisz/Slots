aaf34b51ce12099a8163727437e4fbb9b44044d1
Alvis Zālamans <alvis.zalamans@gmail.com>
2020-06-22 17:36:24 +0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Slots';
+
+  imageList = [
+    {id: 1, src: './assets/Reel/3xBAR.png'},
+    {id: 2, src: './assets/Reel/BAR.png'},
+    {id: 3, src: './assets/Reel/2xBAR.png'},
+    {id: 4, src: './assets/Reel/7.png'},
+    {id: 5, src: './assets/Reel/Cherry.png'}
+  ];
+
+  shuffleArray = arr => arr
+    .map(a => [Math.random(), a])
+    .sort((a, b) => a[0] - b[0])
+    .map(a => a[1])
+
+  shuffle(){
+    this.imageList = this.shuffleArray(this.imageList);
+    console.log(this.imageList)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        NgbModule,
+        MatButtonModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
